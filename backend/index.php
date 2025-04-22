4b2c38db96e266fdd1f32fb7bb00eb621ab4c4d6
Iago Henrique <48492576+iagohnunes@users.noreply.github.com>
2025-04-22 15:42:31 -0300

    chore: alteraçao variável

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,3 @@
 <?php
 header("Content-Type: application/json");
-echo "API funcionando " . $_ENV('name-project') . " ou " . $_GET('name-project');
+echo "API funcionando " . $_ENV['name-project'] . " ou " . $_GET['name-project'];
