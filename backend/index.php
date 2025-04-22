96fe0e16a401b107514a230f2b662e066f998473
Iago Henrique <48492576+iagohnunes@users.noreply.github.com>
2025-04-22 16:23:18 -0300

    chore: alteraçao variável 0.4

--- backend/index.php
+++ backend/index.php
@@ -4,9 +4,12 @@ header("Content-Type: application/json");
 
 $apiKey = getenv("name-project") ?? 'não encontrada';
 echo "API $apiKey";
+echo "<br>";
 
 $apiKey = $_ENV['name-project'] ?? 'não encontrada';
 echo "API $apiKey";
+echo "<br>";
 
 $apiKey = $_SERVER['name-project'] ?? 'não encontrada';
 echo "API $apiKey";
+echo "<br>";
