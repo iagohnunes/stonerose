a550b0594f9a410598f0a2fbd26258de5ae6e66f
Iago Henrique <48492576+iagohnunes@users.noreply.github.com>
2025-04-23 11:51:17 -0300

    chore: alteraçao variável 0.7

--- backend/index.php
+++ backend/index.php
@@ -1,7 +1,23 @@
 <?php
 
-header("Content-Type: application/json");
+<?php
+// CORS
+header("Access-Control-Allow-Origin: *"); // ou coloque o domínio específico
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
-$apiKey = getenv("NAME_PROJECT") ?? 'não encontrada';
-echo "API $apiKey";
-echo "\n<br>";
+use Util\RoutesUtil;
+use Validator\RequestValidator;
+
+include 'bootstrap.php';
+
+try {
+    $RequestValidator = new RequestValidator(RoutesUtil::getRoutes());
+    return $RequestValidator->processRequest();
+} catch (\Throwable $th) {
+    echo $th->getMessage();
+}
