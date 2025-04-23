c0ee61f2eee3be6f1e92640033949cb775c3cfad
Iago Henrique <48492576+iagohnunes@users.noreply.github.com>
2025-04-23 15:53:08 -0300

    style: add

--- frontend/index.php
+++ frontend/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="frontend/styles.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
 
 </head>
 
